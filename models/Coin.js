1c6962c587692f9c856c7bb268e42cb7c743447e
asphub <s.ajith@infospica.com>
2018-03-22 10:08:06 +0530

    Code Formated

--- models/Coin.js
+++ models/Coin.js
@@ -2,15 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // Define collection and schema for Items
-var Coin = new Schema({
-  name: {
-    type: String
-  },
-  price: {
-    type: Number
-  }
-},{
+var Coin = new Schema(
+  {
+    name: {
+      type: String
+    },
+    price: {
+      type: Number
+    }
+  }, {
     collection: 'coins'
-});
+  }
+);
 
 module.exports = mongoose.model('Coin', Coin);
