0a2716e9e9d6e24a4be4c6bebeec86ffd05e69c8
asphub <ajith2007plr@gmail.com>
2018-03-22 10:12:34 +0530

    Code Formated

--- expressRoutes/coinRoutes.js
+++ expressRoutes/coinRoutes.js
@@ -8,19 +8,19 @@ var Coin = require('../models/Coin');
 // Defined store route
 coinRoutes.route('/add').post(function (req, res) {
   var coin = new Coin(req.body);
-   coin.save()
+  coin.save()
     .then(item => {
-    res.status(200).json({'coin': 'Coin added successfully'});
+      res.status(200).json({ 'coin': 'Coin added successfully' });
     })
     .catch(err => {
-    res.status(400).send("unable to save to database");
+      res.status(400).send("Unable to save to database");
     });
 });
 
 // Defined get data(index or listing) route
 coinRoutes.route('/').get(function (req, res) {
-   Coin.find(function (err, coins){
-    if(err){
+  Coin.find(function (err, coins) {
+    if (err) {
       console.log(err);
     }
     else {
@@ -32,14 +32,14 @@ coinRoutes.route('/').get(function (req, res) {
 // Defined edit route
 coinRoutes.route('/edit/:id').get(function (req, res) {
   var id = req.params.id;
-  Coin.findById(id, function (err, coin){
-      res.json(coin);
+  Coin.findById(id, function (err, coin) {
+    res.json(coin);
   });
 });
 
 //  Defined update route
 coinRoutes.route('/update/:id').post(function (req, res) {
-   Coin.findById(req.params.id, function(err, coin) {
+  Coin.findById(req.params.id, function (err, coin) {
     if (!coin)
       return next(new Error('Could not load Document'));
     else {
@@ -47,21 +47,21 @@ coinRoutes.route('/update/:id').post(function (req, res) {
       coin.price = req.body.price;
 
       coin.save().then(coin => {
-          res.json('Update complete');
+        res.json('Update complete');
       })
-      .catch(err => {
-            res.status(400).send("unable to update the database");
-      });
+        .catch(err => {
+          res.status(400).send("Unable to update the database");
+        });
     }
   });
 });
 
 // Defined delete | remove | destroy route
 coinRoutes.route('/delete/:id').get(function (req, res) {
-   Coin.findByIdAndRemove({_id: req.params.id}, function(err, coin){
-        if(err) res.json(err);
-        else res.json('Successfully removed');
-    });
+  Coin.findByIdAndRemove({ _id: req.params.id }, function (err, coin) {
+    if (err) res.json(err);
+    else res.json('Successfully removed');
+  });
 });
 
 module.exports = coinRoutes;
